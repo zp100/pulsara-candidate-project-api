2af506cb4f0874c54133b5c367b04706b945a82b
Zach Preston <zachattack6089@gmail.com>
2024-01-11 11:35:09 -0800

    Added setup and boilerplate.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # pulsara-candidate-project-api
 Back-end canditate project for Pulsara's "Full Stack Software Engineer" position.
+
+## Setup
+
+### Requirements
+-   Python 3.10 or later
+-   `pip` and `git`
+
+### Download
+Clone this GitHub repository:
+```
+git clone https://github.com/zp100/pulsara-candidate-project-api.git
+cd pulsara-candidate-project-api
+```
+
+### Docker and MySQL
+Install the Docker image, then run it as a container. This will host the MySQL database locally on port 3306. The database can be connected to via the Docker container's shell:
+```
+mysql --host=localhost --user=root --password=password main
+```
+You can now directly query the `main` database. For example, to list all tables in the database:
+```
+mysql> show tables;
++--------------------------+
+| Tables_in_main           |
++--------------------------+
+| TBL_ENTITIES             |
+| TBL_ENTITY_RELATIONSHIPS |
++--------------------------+
+2 rows in set (0.01 sec)
+
+```
+
+### Python
+Install the required Python package dependencies for this project. If you want to avoid installing these packages globally, first create a virtual environment:
+```
+python3 -m venv .env
+source .env/bin/activate
+```
+Whether the Python virtual environment is created or not, next install the dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Running
+Once the installations are complete, you can run the program by hosting a local server:
+```
+flask run
+```
+Then you can access the project at http://127.0.0.1:5000, such as with a browser.
---
+++ app.py
@@ -0,0 +1,61 @@
+################################################################
+#   File:   app.y
+#   Desc:   Python Flask RESTful back-end for managing
+#           requests.
+#   Author: Zachariah Preston
+#   Date:   2024-01-11
+################################################################
+
+# Import packages.
+import flask
+import mysql.connector as sql
+
+# App setup.
+app = flask.Flask(__name__)
+
+
+
+################################################################
+#   Func:   root
+#   Desc:   Route function for root URL.
+#   Params: None
+#   Ret:    <flask.Response>: HTTP response that contains the
+#               queried data as a JSON object.
+################################################################
+@app.route('/', methods=['GET'])
+def root():
+    # Connect to the MySQL database.
+    conn = sql.connect(
+        host='localhost',
+        user='root',
+        password='password',
+        database='main',
+    )
+    cur = conn.cursor()
+
+    # DEBUG
+    # Do a sample query that gets the entities in the database.
+    cur.execute("""
+        select entity_type, name
+        from TBL_ENTITIES;
+    """)
+
+    # Loop through the entries to sort them into hospitals and EMS's.
+    etys = {
+        'hospital_list': [],
+        'ems_list': [],
+    }
+    for (entity_type, name) in cur:
+        # Check which type of entity it is.
+        if entity_type == 'HOSPITAL':
+            # Add to hospital list.
+            etys['hospital_list'].append(name)
+        elif entity_type == 'EMS':
+            # Add to EMS list.
+            etys['ems_list'].append(name)
+        else:
+            # Error.
+            raise Exception(f"unrecognized entity type for entity with name {name}")
+
+    # Return the data as a JSON response.
+    return flask.jsonify(etys)
