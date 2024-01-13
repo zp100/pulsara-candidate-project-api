7847835ca92740c82e9d2868feda82f1e9decf61
Zach Preston <zachattack6089@gmail.com>
2024-01-13 13:07:16 -0800

    Added some error messages to be used later.

--- app.py
+++ app.py
@@ -24,14 +24,19 @@ app = flask.Flask(__name__)
 ################################################################
 @app.route('/', methods=['GET'])
 def root():
-    # Connect to the MySQL database.
-    conn = sql.connect(
-        host='localhost',
-        user='root',
-        password='password',
-        database='main',
-    )
-    cur = conn.cursor()
+    # Test that the database is accessible.
+    try:
+        # Connect to the MySQL database.
+        conn = sql.connect(
+            host='localhost',
+            user='root',
+            password='password',
+            database='main',
+        )
+        cur = conn.cursor()
+    except sql.errors.DatabaseError:
+        # Couldn't connect to database.
+        flask.abort(503, 'Server failed to connect to the SQL database.')
 
     # DEBUG
     # Do a sample query that gets the entities in the database.
@@ -55,7 +60,7 @@ def root():
             etys['ems_list'].append(name)
         else:
             # Error.
-            raise Exception(f"unrecognized entity type for entity with name {name}")
+            flask.abort(400, f"Unrecognized entity type of \"{entity_type}\" for entity with name \"{name}\".")
 
     # Return the data as a JSON response.
     return flask.jsonify(etys)
